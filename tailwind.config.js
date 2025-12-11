8327c03fc3d4e407d7ae944a25882192325316d4
dikrifm <dikrifm@github.com>
2025-12-11 22:20:16 +0700

    Chore: Update Tailwind config to scan App/Cells and rebuild CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/Views/**/*.php",
-    "./app/Cells/**/*.php",       // Tambahan: Scan Logic Cell
-    "./app/Controllers/**/*.php",
-    "./public/js/**/*.js"
+    "./app/Views/**/*.php",       // Memindai semua view (Admin, Public, Layout)
+    "./app/Cells/**/*.php",       // PENTING: Memindai logic Cell (karena ada SVG icon di sana)
+    "./app/Controllers/**/*.php", // Memindai Controller (jika ada dynamic class)
+    "./public/js/**/*.js"         // Memindai file JS (Alpine/Custom)
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
+        sans: ['Inter', 'sans-serif'], // Standarisasi Font Inter
       },
     },
   },
